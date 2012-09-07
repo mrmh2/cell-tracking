55a098ba0bfaf7100513cd134e8d8a3addd3a20e
Matthew Hartley <Matthew.Hartley@jic.ac.uk>
2012-09-07 15:26:21 +0100

    Some non functional stuff with multiprocessing

--- src/tracker/profiler.py
+++ src/tracker/profiler.py
@@ -2,6 +2,7 @@
 
 import celldata
 import threading
+import multiprocessing
 
 class CellLoader(threading.Thread):
     def __init__(self, ifile):
@@ -15,7 +16,20 @@ class CellLoader(threading.Thread):
     def cd(self):
         return self.celld
 
-def main():
+class PCellLoader(multiprocessing.Process):
+    def __init__(self, ifile):
+        self.ifile = ifile
+        multiprocessing.Process.__init__(self)
+        self.celld = None
+
+    def run(self):
+        self.celld = celldata.CellData(self.ifile)
+
+    @property
+    def cd(self):
+        return self.celld
+
+def threaded_main():
     ifile1 = 'data/newexp/segmented_image/T05.png'
     #cd1 = celldata.CellData(ifile1)
 
@@ -43,6 +57,33 @@ def main():
 
     #print cd1[20].centroid
     #print cd2[21].centroid
+
+def main():
+    ifile1 = 'data/newexp/segmented_image/T05.png'
+    ifile2 = 'data/newexp/segmented_image/T06.png'
+
+    ml1 = PCellLoader(ifile1)
+    ml2 = PCellLoader(ifile2)
+    ml3 = PCellLoader(ifile2)
+    ml4 = PCellLoader(ifile2)
+    ml5 = PCellLoader(ifile2)
+    
+    ml1.start()
+    ml2.start()
+    ml3.start()
+    ml4.start()
+    ml5.start()
+
+    ml1.join()
+    ml2.join()
+    ml3.join()
+    ml4.join()
+    ml5.join()
+
+    cd1 = ml5.cd
+
+    print cd1[20].centroid
+    
 
 if __name__ == '__main__':
     main()
